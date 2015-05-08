9768d4869ecc6677f7dcae03a98de7e0da63633d
Nick Reid <nick3reid@gmail.com>
2015-05-07 21:05:51 -0500

    Added player death

---
+++ Assets/Scripts/Game Scripts/Gameplay/Items/Explosion.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.GetComponent<Health>().KillPlayer();
+		}
+	}
+
+}
