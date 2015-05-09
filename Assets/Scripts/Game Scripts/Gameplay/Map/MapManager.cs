631030fcd65bcb0ebc61624b7f7be4f7c50204a7
Denny Scott <DennyScott301@gmail.com>
2015-05-09 12:11:51 -0500

    Added Floor Plan

--- Assets/Scripts/Game Scripts/Gameplay/Map/MapManager.cs
+++ Assets/Scripts/Game Scripts/Gameplay/Map/MapManager.cs
@@ -39,12 +39,11 @@ public class MapManager : MonoBehaviour {
 	#region Private Methods
 	void CalculateMapSizes() {
 		WallWidth = Wall.transform.localScale.x;
-		WallHeight = Wall.transform.localScale.y;
-		FloorWidth = WallHeight;
-		FloorHeight = WallHeight;
+		WallHeight = Wall.transform.localScale.z;
+		FloorWidth = WallWidth;
+		FloorHeight = WallWidth;
 		_cubeHeight = FloorHeight/VerticalCubes;
 		_cubeWidth = FloorHeight/HorizontalCubes;
-		
 	}
 
 	void CreateGrid() {
@@ -56,8 +55,22 @@ public class MapManager : MonoBehaviour {
 	void CreateRow(int row) {
 		for (var i = 0; i < HorizontalCubes; i++) {
 			var square = Instantiate(GridSquare);
+			var gridSquareRenderer = GridSquare.GetComponent<Renderer>().bounds.size;
+			square.transform.localScale = new Vector3(gridSquareRenderer.x, 1, gridSquareRenderer.z);
 			_map[row, i] = square;
+			PlaceSquare(row, i, square);
 		}
+	}
+
+	void PlaceSquare(int row, int col, GameObject square) {
+		var WallBuffer = WallWidth/2.0f; //Half the size of the wall, to take from center to edge.
+		var SquareSize = square.transform.localScale.x;
+		Debug.Log(GridSquare.transform.localScale.x);
+		var verticalPosition = (FloorHeight*SquareSize) / (2.0f * GridSquare.transform.localScale.x) - row * SquareSize - WallBuffer * (2 * GridSquare.transform.localScale.x);
+		var horizontalPosition = (FloorHeight*SquareSize) / (2.0f * GridSquare.transform.localScale.x) - col * SquareSize - WallBuffer * (2 * GridSquare.transform.localScale.x);
+		square.SetActive(true);
+		square.transform.position = new Vector3(verticalPosition, 0.0f, horizontalPosition);
+
 	}
 	#endregion
 
